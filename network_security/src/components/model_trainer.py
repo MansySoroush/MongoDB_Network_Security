69bb50f1f76053ea445b97d7ff470c3c63c065cc
MansySoroush <mansy.soroush@gmail.com>
2025-01-28 13:48:11 +0800

    Model Experiment Tracking with MLFlow

--- network_security/src/components/model_trainer.py
+++ network_security/src/components/model_trainer.py
@@ -22,6 +22,8 @@ from sklearn.ensemble import (
     RandomForestClassifier,
 )
 
+import mlflow
+
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
         try:
@@ -29,6 +31,17 @@ class ModelTrainer:
             self.data_transformation_artifact=data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+
+    def track_mlflow(self,best_model,classification_metric):
+        with mlflow.start_run():
+            f1_score=classification_metric.f1_score
+            precision_score=classification_metric.precision_score
+            recall_score=classification_metric.recall_score            
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
 
     def train_model(self,X_train,y_train,x_test,y_test):
         models = {
@@ -79,9 +92,15 @@ class ModelTrainer:
         y_train_pred=best_model.predict(X_train)
 
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
-        
+
+        # Track the experiments with mlflow
+        self.track_mlflow(best_model,classification_train_metric)
+
         y_test_pred=best_model.predict(x_test)
         classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
+
+        # Track the experiments with mlflow
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
             
@@ -98,7 +117,6 @@ class ModelTrainer:
                             )
         logging.info(f"Model trainer artifact: {model_trainer_artifact}")
         return model_trainer_artifact
-
 
     def initiate_model_trainer(self)->ModelTrainerArtifact:
         try:
